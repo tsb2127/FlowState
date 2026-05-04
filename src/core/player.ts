333c90f8739dbdfbba7a0f99799c989120881277
Tanmay Bhalerao <tsb2127@gmail.com>
2026-05-03 19:46:49 -0500

    Delete src directory

--- src/core/player.ts
+++
@@ -1,9 +0,0 @@
-import type { Session } from './flowEngine';
-
-export interface PlayerProgress {
-  totalXp: number;
-}
-
-export function calculateTotalXp(sessions: Session[]): number {
-  return sessions.reduce((total, session) => total + session.xpEarned, 0);
-}
